08d11a912fb8d01a6857ee0fe93382ccc8804def
Olusolaayomi <sshollybaby1@gmail.com>
2022-09-11 05:11:52 -0700

    my first commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,12 +11,12 @@ int main(void)
 
 	for (i = 48; i < 58; i++)
 	{
-		putchar (i);
+		putchar(i);
 	}
 	for (i = 97; i < 103; i++)
 	{
-		putchar (i);
+		putchar(i);
 	}
-	putchar ('\n');
+	putchar('\n');
 	return (0);
 }
