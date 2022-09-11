72e0545c659ce30921cefe1182a9d59a11a4894d
Olusolaayomi <sshollybaby1@gmail.com>
2022-09-11 09:57:30 -0700

    my first commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print single digits numbers
+ *
+ * Description: print single digits numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
