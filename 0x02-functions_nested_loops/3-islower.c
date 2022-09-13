49223107fd032069904606e1fd9f53b9918f65b9
Olusolaayomi <sshollybaby1@gmail.com>
2022-09-13 14:54:46 -0700

    my first commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,13 +3,11 @@
 /**
  * _islower - prints 1 or 0 depending on input
  * @c: first parameter
- * 
- * Description: print all lowercase letters
+ * Description: prints all lowercase letters
  * Return: Always(0).
  */
 
 int _islower(int c)
 {
-	return(c >= 'a' && c <='z');
+	return (c >= 'a' && c <= 'z');
 }
-
