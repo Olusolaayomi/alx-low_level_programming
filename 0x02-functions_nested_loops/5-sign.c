b6ac342ce8b8c11634191a7c412588a16d89c11c
Olusolaayomi <sshollybaby1@gmail.com>
2022-09-13 17:03:15 -0700

    my fist commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *_print_sign - print the sign of a number
+ *print_sign - Print the sign of a number
  * @n: type int integer, can be negative or positive
  * Description: print +, 0, or - depending on number  along with return
  * Return: 1 if +, 0 if 0, and -1 if -
